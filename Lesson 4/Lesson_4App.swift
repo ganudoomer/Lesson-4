7ab741f48a6b5259321e199725d5674741e1b893
ganudoomer <ganuiscool@gmail.com>
2021-08-23 23:14:22 +0530

    Initial Commit

---
+++ Lesson 4/Lesson_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4App.swift
+//  Lesson 4
+//
+//  Created by Sree on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
